67d54a097dbba930f6673c00c3d632b9755ca729
Johan Grahn <johan@cgeek.net>
2011-03-10 00:30:36 +0100

    Added better debugging information with replica

--- src/Debug.h
+++ src/Debug.h
@@ -19,13 +19,13 @@
 #include "Config.h"
 
 #ifndef NDEBUG
-	#define __DEBUG(fmt, ...) fprintf (__conf.log, "[%d] " fmt "\n", __conf.id, ##__VA_ARGS__ ); fflush( __conf.log )
-	#define __WARNING(fmt, ...) fprintf (__conf.log, "\n[%d] WARNING: " fmt "\n\n", __conf.id, ##__VA_ARGS__ ) ; fflush( __conf.log )
+	#define __DEBUG(fmt, ...) fprintf (__conf.log, "[Replica %d] " fmt "\n", __conf.id, ##__VA_ARGS__ ); fflush( __conf.log )
+	#define __WARNING(fmt, ...) fprintf (__conf.log, "\n[Replica %d] WARNING: " fmt "\n\n", __conf.id, ##__VA_ARGS__ ) ; fflush( __conf.log )
 #else
 	#define __DEBUG(fmt, ...) 
 	#define __WARNING(fmt, ...) 
 #endif
 
-#define __ERROR(fmt, ...) fprintf (__conf.log, "[%d] ERROR: " fmt "\n", __conf.id, ##__VA_ARGS__ ) ; fflush( __conf.log )
-#define __TIME(fmt, ...) fprintf( __conf.log, "[%d] TIME: " fmt " seconds \n",  __conf.id, ##__VA_ARGS__ ) ; fflush( __conf.log )
-#define __PRINT(fmt, ...) fprintf (__conf.log, "[%d] " fmt "\n", __conf.id, ##__VA_ARGS__ ); fflush( __conf.log )
+#define __ERROR(fmt, ...) fprintf (__conf.log, "[Replica %d] ERROR: " fmt "\n", __conf.id, ##__VA_ARGS__ ) ; fflush( __conf.log )
+#define __TIME(fmt, ...) fprintf( __conf.log, "[Replica %d] TIME: " fmt " seconds \n",  __conf.id, ##__VA_ARGS__ ) ; fflush( __conf.log )
+#define __PRINT(fmt, ...) fprintf (__conf.log, "[Replica %d] " fmt "\n", __conf.id, ##__VA_ARGS__ ); fflush( __conf.log )
